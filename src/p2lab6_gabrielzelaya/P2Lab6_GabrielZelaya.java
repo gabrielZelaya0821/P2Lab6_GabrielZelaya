ac049198a7c6e729a8d90950430b4db4d6ae982a
gabrielZelaya0821 <gabriel.zelaya2005@gmail.com>
2023-03-03 13:15:40 -0600

    Spotify

---
+++ src/p2lab6_gabrielzelaya/P2Lab6_GabrielZelaya.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package p2lab6_gabrielzelaya;
+
+/**
+ *
+ * @author gabri
+ */
+public class P2Lab6_GabrielZelaya {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
